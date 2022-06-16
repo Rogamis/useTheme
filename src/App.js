4509fb98fe2b58eb4428b726f8867969ac785c95
Bogdan <roug.rogamis@gmail.com>
2022-06-16 16:43:10 +0300

    useTheme-hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Form from "react-bootstrap/Form";
+import { VscCode } from "react-icons/vsc";
+import { useTheme } from "./hooks/use-theme";
 
-function App() {
+import "./App.css";
+
+export default function App() {
+  const { theme, setTheme } = useTheme();
+
+  const handleLightThemeClick = () => {
+    setTheme("light");
+  };
+  const handleDarkThemeClick = () => {
+    setTheme("dark");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container fluid d-flex flex-column">
+      <div className="p-3 d-flex justify-content-end">
+        <ButtonGroup aria-label="Theme toggle">
+          <Button variant="secondary" onClick={handleLightThemeClick}>
+            Light
+          </Button>
+          <Button variant="secondary" onClick={handleDarkThemeClick}>
+            Dark
+          </Button>
+        </ButtonGroup>
+      </div>
+      <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
+        <VscCode size={100} className="app__logo mb-5" />
+        <div className="d-flex">
+          <Form.Control
+            className="app__input mr-3"
+            type="email"
+            placeholder="..."
+          />
+          <Button className="app__button" type="button">
+            List
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
